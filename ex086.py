11cc981769203a02cbb292297f5a19c2781b5451
Rafael Bernal <rafaelbernal873@gmail.com>
2026-09-02 11:39:57 -0300

    Adicionando exercícios 83 a 87

---
+++ ex086.py
@@ -0,0 +1,34 @@
+from random import randint
+from time import sleep
+
+lista = []
+jogos = []
+cont = 0
+
+print('-'*30)
+print('     JOGO DA MEGA SENA!     ')
+print('-'*30)
+
+quant = int(input('Quantos jogos deseja jogar: '))
+tot = 1
+while tot <= quant:
+    cont = 0
+    while True:
+        num = randint(1, 60)
+        if num not in lista:
+            cont += 1
+            lista.append(num)
+        if cont >=6:
+            break
+
+    lista.sort()
+    jogos.append(lista[:])
+    lista.clear()
+    tot += 1
+
+print('=-' * 3, f' SORTEANDO {quant} JOGOS ', '=-' * 3)
+for i, l in enumerate(jogos):
+    print(f'Jogo {i+1}: {l}')
+    sleep(1)
+print('=-' * 3, f' BOA SORTE!! ', '=-' * 3)
+
